b5b3e5cd8ac0a4b665f904c45a8cc9cad3971f4b
Memmen <brian.memmen@gmail.com>
2019-05-11 12:35:00 -0400

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Brian Memmen on 5/11/19.
+//  Copyright © 2019 Brian Memmen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
